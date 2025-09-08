d6cded973c41a90064960a11ab180f84de53486e
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-09-08 06:56:36 +0530

    update app.js

--- Src/00 YT/JVL/03 Creating Server/app.js
+++ Src/00 YT/JVL/03 Creating Server/app.js
@@ -1,8 +1,30 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit(); // Stop Event 
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Form Details</title></head>");
+    res.write(`<body>
+      <form action='/message' method='POST'>
+         <input type='text' name='message'/>
+         <input type='submit' value="Send"/>
+      </form>
+    </body>`);
+    // res.write(
+    //   '<body><form action="/message" method="POST"><input type="text" name="message" /><input type="submit" value="Send"/></form></body>'
+    // );
+
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node Js Tutorial</title></head>");
+  res.write("<body><h1>Hello from Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
